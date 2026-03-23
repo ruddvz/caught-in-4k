b11d1fa4b44d4cdd582de405fde0aacf6c619152
ruddvz <pvr6675@gmail.com>
2026-03-23 00:30:43 -0400

    Fix mobile search visibility, top nav padding, and profile avatar integration

--- src/components/NavBar/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/NavBar/TopNavigationBar/TopNavigationBar.tsx
@@ -38,6 +38,38 @@ const TopNavigationBar = memo(({ className, route, query, tabs }: Props) => {
         window.location.hash = currentUrl.hash;
     }, []);
 
+    const [currentProfile, setCurrentProfile] = React.useState<any>(null);
+
+    React.useEffect(() => {
+        const stored = localStorage.getItem('c4k_current_profile');
+        if (stored) {
+            try {
+                setCurrentProfile(JSON.parse(stored));
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    }, []);
+
+    const avatars = [
+        require('../../../assets/images/avatars/abstract-avatar-1-circle-x3ecvpg9e9e3jtdw7ny2xs.png'),
+        require('../../../assets/images/avatars/abstract-avatar-2-circle-rfv7tepq8onk3cwh4hta4d.png'),
+        require('../../../assets/images/avatars/abstract-avatar-3-circle-0yg49l6kn99g7py9q5r7gj.png'),
+        require('../../../assets/images/avatars/abstract-avatar-4-circle-nb6qd1yx1gas0k26aq3jrf.png'),
+        require('../../../assets/images/avatars/abstract-avatar-5-circle-u6e0p97idsipndksw2lcaq.png'),
+        require('../../../assets/images/avatars/abstract-avatar-6-circle-9xlbldchfaeckxehv4tjpk.png'),
+        require('../../../assets/images/avatars/abstract-avatar-7-circle-76pjyw5fm6gdvqjgyuort.png'),
+        require('../../../assets/images/avatars/abstract-avatar-8-circle-83i2c7b13kbvq9x6crxf.png'),
+        require('../../../assets/images/avatars/abstract-avatar-9-circle-bugfxnw039lgvn25kqzo4c.png'),
+        require('../../../assets/images/avatars/abstract-avatar-10-circle-lrsrjd2txm8mkkolw7902.png'),
+        require('../../../assets/images/avatars/abstract-avatar-11-circle-7apdsvuf90sqtke9ybhz3.png'),
+        require('../../../assets/images/avatars/abstract-avatar-12-circle-k7p9le9zztnhp0wfjxzcpl.png'),
+        require('../../../assets/images/avatars/abstract-avatar-13-circle-xwe9z0rcnakp9ehxtqpqp.png'),
+        require('../../../assets/images/avatars/abstract-avatar-14-circle-iflsbo952sl97g9h1c8uqb.png'),
+        require('../../../assets/images/avatars/abstract-avatar-15-circle-lges9w2kuam0yxwl9s51ixd.png'),
+        require('../../../assets/images/avatars/abstract-avatar-16-circle-dcwuud5dq7kw3o25bu1qz8.png')
+    ];
+
     return (
         <nav className={classnames(className, styles['top-nav-bar-container'])}>
             <div className={styles['left-section']}>
@@ -61,7 +93,7 @@ const TopNavigationBar = memo(({ className, route, query, tabs }: Props) => {
                 ) : (
                     <div className={styles['nav-pill-wrapper']}>
                         <div className={styles['nav-pill']}>
-                            {tabs.slice(0, 6).map((tab) => (
+                            {tabs.map((tab) => (
                                 <Button 
                                     key={tab.id}
                                     href={tab.href}
@@ -84,8 +116,15 @@ const TopNavigationBar = memo(({ className, route, query, tabs }: Props) => {
                 >
                     <Icon name={fullscreen ? 'minimize' : 'maximize'} className={styles['icon']} />
                 </Button>
-                <Button className={styles['action-btn']} href="#/profiles">
-                    <Icon name="person-outline" className={styles['icon']} />
+                <Button className={classnames(styles['action-btn'], styles['profile-btn'])} href="#/profiles">
+                    {currentProfile ? (
+                        <div 
+                            className={styles['profile-avatar']} 
+                            style={{ backgroundImage: `url(${avatars[currentProfile.avatarIndex]})` }} 
+                        />
+                    ) : (
+                        <Icon name="person-outline" className={styles['icon']} />
+                    )}
                 </Button>
             </div>
         </nav>
