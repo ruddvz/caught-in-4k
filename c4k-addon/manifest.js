47c30e6f3e1ac6680b0d000f950bfd4073c93280
ruddvz <pvr6675@gmail.com>
2026-09-23 21:30:59 -0400

    feat(addon): add C4K Stremio manifest

---
+++ c4k-addon/manifest.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const manifest = Object.freeze({
+    id: 'live.c4k.quality',
+    version: '0.1.0',
+    name: 'C4K Quality',
+    description: 'Quality-first stream resolver for authorised C4K movie libraries.',
+    resources: [
+        {
+            name: 'stream',
+            types: ['movie'],
+            idPrefixes: ['tt'],
+        },
+    ],
+    types: ['movie'],
+    catalogs: [],
+    idPrefixes: ['tt'],
+    behaviorHints: {
+        adult: false,
+        p2p: false,
+        configurable: false,
+    },
+});
+
+module.exports = { manifest };
